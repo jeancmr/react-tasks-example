09ac191c6e850f7f6b17fa43a9d305b92b47ea4e
jeancmr <jean.madiedo08@gmail.com>
2024-01-27 10:29:38 -0500

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://madzals.github.io/react-tasks-example/",
+  base: "https://jeancmr.github.io/react-tasks-example/",
 });
